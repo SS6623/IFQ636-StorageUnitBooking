894ed9f89c9185e49083f1e1f075a4cf9770fdb3
Sweta Shah <sweta.shah@connect.qut.edu.au>
2026-05-29 11:54:45 +1000

    testing IP

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { API_URL } from "../config";
+const API_URL = "http://54.79.136.187:5001";
+//import { API_URL } from "../config";
 
 export default function Dashboard() {
   const [units, setUnits] = useState([]);

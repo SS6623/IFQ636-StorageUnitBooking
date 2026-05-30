617d19d6ebd51d6b7b117f6e9b2e396d46cfdec5
Sweta Shah <sweta.shah@connect.qut.edu.au>
2026-05-30 10:30:18 +1000

    changed Public IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
 
-  baseURL: "http://54.79.136.187:5001",
+  baseURL: "http://13.210.124.59:5001",
   headers: { 'Content-Type': 'application/json' },
 });
 
